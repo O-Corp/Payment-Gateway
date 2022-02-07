2c00292171594b26052844fb1ced090d3ba8fb93
OnamChilwan <onamch@gmail.com>
2022-02-07 18:49:12 +0000

    Added skeleton for component test library and started implementing tests

---
+++ tests/PaymentGateway.Web.Api.ComponentTests/Features/HealthCheck.feature
@@ -0,0 +1,8 @@
+﻿Feature: HealthCheck
+	Health Checks
+
+@healthcheck
+Scenario: Successful Health Check
+	Given all dependencies are healthy
+	When a health check request is performed
+	Then an OK health check response is returned
---
+++ tests/PaymentGateway.Web.Api.ComponentTests/Steps/HealthCheckSteps.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using TechTalk.SpecFlow;
+
+namespace PaymentGateway.Web.Api.ComponentTests.Steps
+{
+    [Binding]
+    public class HealthCheckSteps
+    {
+        [Given(@"all dependencies are healthy")]
+        public void GivenAllDependenciesAreHealthy()
+        {
+        }
+
+        [When(@"a health check request is performed")]
+        public void WhenAHealthCheckRequestIsPerformed()
+        {
+        }
+
+        [Then(@"an (.*) health check response is returned")]
+        public void ThenAHealthCheckResponseIsReturned(HttpStatusCode httpStatusCode)
+        {
+        }
+    }
+}
