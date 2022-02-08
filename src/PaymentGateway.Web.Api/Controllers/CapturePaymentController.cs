970658b415288be4e83175c31b97e0e77d8a548e
OnamChilwan <onamch@gmail.com>
2022-02-08 09:47:52 +0000

    Moved validation into a separate folder

--- src/PaymentGateway.Web.Api/Controllers/CapturePaymentController.cs
+++ src/PaymentGateway.Web.Api/Controllers/CapturePaymentController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PaymentGateway.Web.Api.Models;
+using PaymentGateway.Web.Api.Validators;
 
 namespace PaymentGateway.Web.Api.Controllers;
 
@@ -29,32 +30,5 @@ public class CapturePaymentController : ControllerBase
         }
 
         return this.Ok();
-    }
-}
-
-public class PaymentRequestValidator
-{
-    public List<ApiError> Validate(CardCaptureRequest request)
-    {
-        var errors = new List<ApiError>();
-        if (!IsAmountValid(request.Amount))
-        {
-            errors.Add(ApiError.Create("InvalidAmount", "Amount is Invalid"));
-        }
-        
-        if (request?.Card == null)
-        {
-            errors.Add(ApiError.Create("InvalidCard", "Card Details are Invalid"));
-        }
-
-        return errors;
-    }
-    
-    private static bool IsAmountValid(decimal amount)
-    {
-        const int minimumPaymentAmount = 1;
-        const int maximumPaymentAmount = 500;
-
-        return amount is >= minimumPaymentAmount and <= maximumPaymentAmount;
     }
 }
