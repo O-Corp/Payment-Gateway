2edf486fe3d33d14ba50beee9025bb939f82f802
OnamChilwan <onamch@gmail.com>
2022-02-08 09:55:42 +0000

    Added unit test project for validation so we can tease out further validation rules

---
+++ tests/PaymentGateway.Web.Api.UnitTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace PaymentGateway.Web.Api.UnitTests;
+
+public class PaymentRequestValidatorTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
