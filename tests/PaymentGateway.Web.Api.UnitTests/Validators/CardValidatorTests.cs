7cbb0f3d257822cce1a8dd7ac8f6506d0633ec8b
OnamChilwan <onamch@gmail.com>
2022-02-08 10:17:34 +0000

    Added name on card validation

--- tests/PaymentGateway.Web.Api.UnitTests/Validators/CardValidatorTests.cs
+++ tests/PaymentGateway.Web.Api.UnitTests/Validators/CardValidatorTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using NUnit.Framework;
 using PaymentGateway.Testing.Shared;
 using PaymentGateway.Web.Api.Models;
@@ -33,6 +34,21 @@ public class CardValidatorTests
         Assert.That(result.Message, Is.EqualTo("Invalid CVV"));
     }
 
+    [TestCase("")]
+    [TestCase(" ")]
+    [TestCase(null)]
+    [TestCase("123")]
+    public void Given_Invalid_Cardholder_Name_Then_Errors_Are_Returned(string name)
+    {
+        var request = new RequestBuilder()
+            .WithValidRequest()
+            .WithCardHolderName(name)
+            .Build();
+        var result = _subject.Validate(request.Card).Single();
+            
+        Assert.That(result.ErrorCode, Is.EqualTo("InvalidCardHolderName"));
+        Assert.That(result.Message, Is.EqualTo("Invalid Cardholder Name"));
+    }  
 }
 
 public class CardValidator
@@ -44,6 +60,11 @@ public class CardValidator
         if (!IsCvvNumberValid(card))
         {
             errors.Add(ApiError.Create("InvalidCvv", "Invalid CVV"));
+        }
+        
+        if (!IsNameOnCardValid(card))
+        {
+            errors.Add(ApiError.Create("InvalidCardHolderName", "Invalid Cardholder Name"));
         }
         
         return errors;
@@ -59,4 +80,15 @@ public class CardValidator
         return int.TryParse(card.Cvv, out _);
     }
 
+    private static bool IsNameOnCardValid(Card card)
+    {
+        if (string.IsNullOrWhiteSpace(card.CardHolderName))
+        {
+            return false;
+        }
+
+        const string pattern = "[A-Za-z]";
+        var regex = new Regex(pattern);
+        return regex.IsMatch(card.CardHolderName);
+    }
 }
